0879d090c88b4fc1a06c1ac916b09476deeaa19b
Aldalaeen <d.yousef71@gmail.com>
2019-07-27 08:20:44 -0700

    herouko deploy

--- src/App.js
+++ src/App.js
@@ -1,55 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import './App.css';
-import './util.css';
-import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
-import Gallery from './components/Gallery.js';
+import './App.css'
+import './util.css'
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap'
+import Gallery from './components/Gallery.js'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       query: '',
       items: []
-    };
+    }
   }
 
-  search() {
-    const API_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
+  search () {
+    const API_URL = 'https://www.googleapis.com/books/v1/volumes?q='
     fetch(`${API_URL}${this.state.query}`)
       .then(response => response.json())
       .then(json => {
-        let {items} = json;
-        this.setState({items})
-      }); // TODO: Add a catch method here in case the API call fails
+        let { items } = json
+        this.setState({ items })
+      }) // TODO: Add a catch method here in case the API call fails
   }
 
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">NYT Google Books Search </h1>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Google Books Search </h1>
         </header>
-        <div className="container main-content">
+        <div className='container main-content'>
           <FormGroup>
             <InputGroup>
-              <FormControl type="text" placeholder="Search for a book"
-              onChange={ event => this.setState({ query: event.target.value }) }
-              onKeyPress={ event => {
-                if ('Enter' === event.key) {
-                  this.search();
+              <FormControl type='text' placeholder='Search for a book'
+                onChange={event => this.setState({ query: event.target.value })}
+                onKeyPress={event => {
+                  if (event.key === 'Enter') {
+                    this.search()
                 }
-              }} />
+                }} />
               <InputGroup.Addon onClick={() => this.search()}>
-                <Glyphicon glyph="search"></Glyphicon>
+                <Glyphicon glyph='search' />
               </InputGroup.Addon>
             </InputGroup>
           </FormGroup>
           <Gallery items={this.state.items} />
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
